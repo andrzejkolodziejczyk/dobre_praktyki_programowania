801d771485c4f6718a3f626c19f8425acbe65579
andrzejkolodziejczyk <andrzej.kolodziejczyk1@edu.uekat.pl>
2025-11-29 15:32:56 +0100

    feat(#86c6u63fz): refactor endpoints to read data from db

--- api/endpoints.py
+++ api/endpoints.py
@@ -1,83 +1,49 @@
-import csv
-from pathlib import Path
+import sys
 from fastapi import APIRouter
+
+sys.path.append("../db")
+
 from models import Movie, Link, Rating, Tag
+from init_db import get_session
 
 router = APIRouter()
 
+
 @router.get('/movies')
 def get_movies():
-    movies = []
-    rows = read_csv_file('movies.csv')
-    
-    for row in rows:
-        movie = Movie(
-            movieId=row['movieId'],
-            title=row['title'],
-            genres=row['genres']
-        )
-        movies.append(movie.__dict__)
-    
-    return movies
+    session = get_session()
+    try:
+        movies = session.query(Movie).all()
+        return movies
+    finally:
+        session.close()
 
 
 @router.get('/links')
 def get_links():
-    links = []
-    rows = read_csv_file('links.csv')
-    
-    for row in rows:
-        link = Link(
-            movieId=row['movieId'],
-            imdbId=row['imdbId'],
-            tmdbId=row['tmdbId']
-        )
-        links.append(link.__dict__)
-    
-    return links
+    session = get_session()
+    try:
+        links = session.query(Link).all()
+        return links
+    finally:
+        session.close()
 
 
 @router.get('/ratings')
 def get_ratings():
-    ratings = []
-    rows = read_csv_file('ratings.csv')
-    
-    for row in rows:
-        rating = Rating(
-            userId=row['userId'],
-            movieId=row['movieId'],
-            rating=row['rating'],
-            timestamp=row['timestamp']
-        )
-        ratings.append(rating.__dict__)
-    
-    return ratings
+    session = get_session()
+    try:
+        ratings = session.query(Rating).all()
+        return ratings
+    finally:
+        session.close()
 
 
 @router.get('/tags')
 def get_tags():
-    tags = []
-    rows = read_csv_file('tags.csv')
-    
-    for row in rows:
-        tag = Tag(
-            userId=row['userId'],
-            movieId=row['movieId'],
-            tag=row['tag'],
-            timestamp=row['timestamp']
-        )
-        tags.append(tag.__dict__)
-    
-    return tags
-
-def read_csv_file(filename: str):
-    """Read a CSV file and return its contents as a list of dictionaries."""
-    csv_path = Path(__file__).parent.parent / 'db' / filename
-    
-    data = []
-    with open(csv_path, 'r', encoding='utf-8') as file:
-        reader = csv.DictReader(file)
-        for row in reader:
-            data.append(row)
-    
-    return data
+    session = get_session()
+    try:
+        tags = session.query(Tag).all()
+        return  tags
+    finally:
+        session.close()
